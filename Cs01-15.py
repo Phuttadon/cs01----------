1acd9cc3f5be8e492de67436f2fb26eaf0769f8f
Phuttadon <87053907+Phuttadon@users.noreply.github.com>
2021-08-18 12:08:38 +0700

    ...

---
+++ Cs01-15.py
@@ -0,0 +1,8 @@
+def my_list(num):
+    for i in num:
+        print(i)
+thislist=["5","10","15","20","25","50","20"]     
+for i in range(len(thislist)):
+    if (thislist[i]=="20"):
+        thislist[i]="200"
+my_list(thislist)
