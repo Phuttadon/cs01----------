c15d16a04657ff485e7d61de18c48ead04f6497d
Phuttadon <87053907+Phuttadon@users.noreply.github.com>
2021-09-22 11:00:11 +0700

    แก้ไข*2

--- Cs01-18.py
+++ Cs01-18.py
@@ -1,6 +1,6 @@
 from tkinter import *
 root=Tk()
-root.title("เครื่องคิดเลข")
+root.title("First GUI")
 myText= Label(text="My name is ",fg="blue",font=20).grid(row=0,column=0)
 myText= Label(text="Phuttadon",fg="red",font=20).grid(row=1,column=1)
 myText= Label(text="Khuntawong",fg="green",font=20).grid(row=3,column=2)
